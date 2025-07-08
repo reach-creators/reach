33fb4300e5f584b2ab40a260ae7ee66bab609630
Maher Mawlawi <maher.mawlawi@gmail.com>
2025-07-08 20:42:14 +0300

    added creator creation

---
+++ backend/src/main/java/com/reach/backend/controllers/CreatorController.java
@@ -0,0 +1,23 @@
+package com.reach.backend.controllers;
+
+import com.example.reach.backend.api.CreatorApi;
+import com.example.reach.backend.dto.CreatorDto;
+import com.example.reach.backend.dto.CreatorInputDto;
+import com.reach.backend.services.CreatorService;
+import com.reach.backend.mappers.CreatorMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CreatorController implements CreatorApi {
+
+  @Autowired private CreatorService creatorService;
+
+  @Override
+  public ResponseEntity<CreatorDto> createCreator(CreatorInputDto creatorInputDto) {
+    return ResponseEntity
+        .status(201)
+        .body(CreatorMapper.MAPPER.toDto(creatorService.create(creatorInputDto)));
+  }
+}
