04272a7dbfa118fd62f3baf583c227d0ce3571b9
Maher Mawlawi <maher.mawlawi@gmail.com>
2025-07-10 20:01:10 +0300

    move toEntity mapping to controller and extract Niche enum to separate schema

--- backend/src/main/java/com/reach/backend/services/CreatorService.java
+++ backend/src/main/java/com/reach/backend/services/CreatorService.java
@@ -2,8 +2,6 @@ package com.reach.backend.services;
 
 import com.reach.backend.domain.tables.Creator;
 import com.reach.backend.repositories.CreatorRepository;
-import com.example.reach.backend.dto.CreatorInputDto;
-import com.reach.backend.mappers.CreatorMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +10,7 @@ public class CreatorService {
 
   @Autowired private CreatorRepository repository;
 
-  public Creator create(CreatorInputDto dto) {
-    Creator entity = CreatorMapper.MAPPER.toEntity(dto);
+  public Creator create(Creator entity) {
     return repository.save(entity);
   }
 }
