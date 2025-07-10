54945bc0a8ffdc687af671d192037ad5660de93d
Mouhamad El Dayeh <dayeh800@gmail.com>
2025-07-10 15:44:40 +0300

    addBrand method in BrandController Fixed

--- backend/src/main/java/com/reach/backend/controllers/BrandController.java
+++ backend/src/main/java/com/reach/backend/controllers/BrandController.java
@@ -6,13 +6,15 @@ import com.reach.backend.services.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class BrandController implements BrandApi {
     @Autowired
     private BrandService brandService;
 
-    public ResponseEntity<BrandCreationDto> createBrand(BrandCreationDto brandCreationDto) {
+    @Override
+    public ResponseEntity<BrandCreationDto> addBrand(@RequestBody BrandCreationDto brandCreationDto) {
         brandService.createBrand(brandCreationDto);
         return ResponseEntity.ok().build();
     }
