2030d10c924615390600f576962fb80dc724897c
Maher Mawlawi <maher.mawlawi@gmail.com>
2025-07-10 21:37:17 +0300

    added update and get for creator

--- backend/src/main/java/com/reach/backend/controllers/CreatorController.java
+++ backend/src/main/java/com/reach/backend/controllers/CreatorController.java
@@ -4,6 +4,7 @@ import com.example.reach.backend.api.CreatorApi;
 import com.example.reach.backend.dto.CreatorDto;
 import com.example.reach.backend.dto.CreatorInputDto;
 import com.reach.backend.services.CreatorService;
+import com.reach.backend.domain.tables.Creator;
 import com.reach.backend.mappers.CreatorMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class CreatorController implements CreatorApi {
 
-  @Autowired private CreatorService creatorService;
+  @Autowired
+  private CreatorService creatorService;
 
   @Override
   public ResponseEntity<CreatorDto> createCreator(CreatorInputDto creatorInputDto) {
@@ -20,5 +22,20 @@ public class CreatorController implements CreatorApi {
     var saved = creatorService.create(entity);
     return ResponseEntity.status(201).body(CreatorMapper.MAPPER.toDto(saved));
   }
+
+  @Override
+  public ResponseEntity<CreatorDto> updateCreator(Integer id, CreatorInputDto dto) {
+    Creator updated = creatorService.update(id, dto);
+    CreatorDto response = CreatorMapper.MAPPER.toDto(updated);
+    return ResponseEntity.ok(response);
+  }
+
+  @Override
+  public ResponseEntity<CreatorDto> getCreatorById(Integer id) {
+      Creator creator = creatorService.getById(id);
+      CreatorDto dto = CreatorMapper.MAPPER.toDto(creator);
+      return ResponseEntity.ok(dto);
+  }
+
 }
 
