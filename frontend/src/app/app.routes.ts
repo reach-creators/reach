01698cfc95a84f6bb69248a487c6b2c11f5f1e6b
elnakouzimohamed <nakouzim2004@gmail.com>
2025-07-12 23:10:10 +0300

    Added Navbar, reoved css files, added tailwindcss

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 import { HealthComponent } from "./components/health/health.component";
 import { SignupComponent } from "./components/signup/signup.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AboutComponent } from "./components/about/about.component";
 
 export const routes: Routes = [
-  { path: '', component: HealthComponent },
+  { path: '', component: NavbarComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  {path: 'about', component: AboutComponent},
 ];
